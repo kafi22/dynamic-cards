57413ca43f8e32d40c2603778e58b072959b31e8
kafi ahmed <kafyahmedfarabi@gmail.com>
2023-01-27 10:07:29 +0600

    something change in app.js

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
-const thePost = document.querySelector('#allPost');
+const thePosts = document.querySelector('#allPost');
 
-const allData = async (config) => {
+const thePOst  = async (config) => {
+   try{
+      const res =  await axios(config)
+      return res.data;
+   }catch(err) {
+      console.log(err);
+   }
+}
+
+
+const postData = async () => {
+
+      let posts = await thePOst('https://jsonplaceholder.typicode.com/posts')
+
+      posts.map(post =>{
+            let createsDiv = document.createElement('div')
+            createsDiv.classList.add('post')
+            createsDiv.innerHTML = `
+            <h4>${post.title}</h4>
+            <p>${post.body}</p>
+            `
+            thePosts.appendChild(createsDiv)
+      })
+
+}
+
+
+postData()
+
+// const allData = async (config) => {
      
-      try{
-            const res = await axios(config)
-            return res.data
-      } catch(err){
-            throw new Error('data could not available')
-      }
+//       try{
+//             const res = await axios(config)
+//             return res.data
+//       } catch(err){
+//             throw new Error('data could not available')
+//       }
 
 
-}
+// }
 
-const showData = async () => {
-     const post = await allData('https://jsonplaceholder.typicode.com/posts');
-     post.map( (data) => {
-      const newPost = document.createElement('div')
-      const newPosts = document.createElement('div')
-      newPost.classList.add('post');
-      newPosts.classList.add('posts')
-      newPost.appendChild(newPosts)
-      newPost.innerHTML = `
-      <h4>${data.title}</h4>
-      <p>${data.body}</p>
-      `
-      thePost.appendChild(newPost)
+// const showData = async () => {
+//      const post = await allData('https://jsonplaceholder.typicode.com/posts');
+//      post.map( (data) => {
+//       const newPost = document.createElement('div')
+//       const newPosts = document.createElement('div')
+//       newPost.classList.add('post');
+//       newPosts.classList.add('posts')
+//       newPost.appendChild(newPosts)
+//       newPost.innerHTML = `
+//       <h4>${data.title}</h4>
+//       <p>${data.body}</p>
+//       `
+//       thePost.appendChild(newPost)
 
-     })
-}
+//      })
+// }
 
-showData()
+// showData()
